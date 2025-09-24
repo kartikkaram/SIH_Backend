9d09596c72193069c48d804b2b1b1c1bc7c41c85
kartik <karambelkarkartik209@gmail.com>
2025-09-24 22:59:28 +0530

    add fetch athletes

--- src/routes/athleteRoutes.js
+++ src/routes/athleteRoutes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { fetchAthleteById } from "../controllers/athletesControllers/fetchAthleteById.js";
 import { updateAthleteById } from "../controllers/athletesControllers/updateAthlete.js";
 import { clerkWebhook } from "../controllers/userControllers/clerk.controller.js";
+import { fetchAthletes } from "../controllers/athletesControllers/fetchAthletes.js";
 
 
 const athleteRouter = express.Router();
 
 athleteRouter.post("/clerk", clerkWebhook )
 athleteRouter.get("/:id", fetchAthleteById )
-athleteRouter.get("/", fetchAthleteById )
+athleteRouter.get("/", fetchAthletes )
 athleteRouter.patch("/:id", updateAthleteById )
 
 
